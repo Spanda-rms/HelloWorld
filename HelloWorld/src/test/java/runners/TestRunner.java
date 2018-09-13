b4416b66f44f5f1b4a3684a1184c9e0984f19402
Spanda-rms <43017336+Spanda-rms@users.noreply.github.com>
2018-09-13 16:04:37 +0100

    commit 8

--- HelloWorld/src/test/java/runners/TestRunner.java
+++ HelloWorld/src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/main/java/resources/FeatureFile.feature",
-//		glue= {"StepDefinition"},
-plugin = { "pretty", "json:target/cucumber-reports/CucumberReport"})
+				glue= {"src/test/java/runners/StepDefinition.java"},
+				plugin = { "pretty", "json:target/cucumber-reports/CucumberReport"})
 public class TestRunner {
 }
